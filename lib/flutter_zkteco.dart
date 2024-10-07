6f4a41cf837dbd653436eb613170f348323f9ae8
Ridwan Fauzan <rymetutor@gmail.com>
2024-10-07 14:28:21 +0700

    added option timeout

--- lib/flutter_zkteco.dart
+++ lib/flutter_zkteco.dart
@@ -22,6 +22,7 @@ import 'package:flutter_zkteco/src/version.dart';
 class ZKTeco {
   String ip;
   int port;
+  Duration timeout;
   late RawDatagramSocket zkClient;
   late StreamController<Datagram> streamController;
 
@@ -29,7 +30,7 @@ class ZKTeco {
   int sessionId = 0;
 
   // Constructor for ZKTeco
-  ZKTeco(this.ip, {this.port = 4370});
+  ZKTeco(this.ip, {this.port = 4370, this.timeout = const Duration(seconds: 10)});
 
   /// Initializes the socket connection to the device.
   ///
@@ -40,7 +41,7 @@ class ZKTeco {
   /// socket, and prints a message to the console when the timeout is hit.
   Future<void> initSocket() async {
     zkClient = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
-    zkClient.timeout(const Duration(seconds: 10), onTimeout: (event) {
+    zkClient.timeout(timeout, onTimeout: (event) {
       debugPrint('Socket timeout');
     });
     streamController = StreamController<Datagram>.broadcast();
@@ -132,18 +133,83 @@ class ZKTeco {
     return int.parse(replyIdHex, radix: 16);
   }
 
+  /// Connects to the ZKTeco device.
+  ///
+  /// This function sends a connect command to the device, which responds with
+  /// a session ID and a checksum. The session ID is stored in the [ZKTeco]
+  /// object, and the checksum is used to verify that the device responded with
+  /// valid data. If the device does not respond or the checksum is invalid, this
+  /// function returns [false]. Otherwise, it returns [true].
+  ///
+  /// This function must be called before any other functions in this class can
+  /// be used.
   Future<bool> connect() => Connect.connect(this);
 
+  /// Disconnects from the ZKTeco device.
+  ///
+  /// This function disconnects from the ZKTeco device using the CMD_EXIT command.
+  ///
+  /// The function takes no arguments and returns a [Future] that completes with a
+  /// [bool] indicating if the device was successfully disconnected, or a [String]
+  /// containing an error message if the device could not be disconnected.
+  ///
+  /// If the device is already disconnected, this function does nothing and returns
+  /// [true].
   Future<bool> disconnect() => Connect.disconnect(this);
 
+  /// Retrieves the version of the device as a [String].
+  ///
+  /// This method sends a command to the device to retrieve its version. The
+  /// device must be connected and authenticated before this method can be
+  /// used.
+  ///
+  /// The method returns a [Future] that completes with a [String] containing
+  /// the version of the device, or a [bool] indicating if the device could not
+  /// be queried.
   Future<dynamic> version() => Version.get(this);
 
+  /// Retrieves the serial number of the device as a [String].
+  ///
+  /// This method sends a command to the device to retrieve its serial
+  /// number. The device must be connected and authenticated before this
+  /// method can be used.
+  ///
+  /// The method returns a [Future] that completes with a [String] containing
+  /// the serial number of the device, or a [bool] indicating if the device
+  /// could not be queried.
   Future<dynamic> serialNumber() => SerialNumber.get(this);
 
+  /// Retrieves the platform of the device as a [String].
+  ///
+  /// This method sends a command to the device to retrieve its platform. The
+  /// device must be connected and authenticated before this method can be
+  /// used.
+  ///
+  /// The method returns a [Future] that completes with a [String] containing
+  /// the platform of the device, or a [bool] indicating if the device could not
+  /// be queried.
   Future<dynamic> platform() => Platform.get(this);
 
+  /// Retrieves the version of the device's platform as a [String].
+  ///
+  /// This method sends a command to the device to retrieve its platform
+  /// version. The device must be connected and authenticated before this
+  /// method can be used.
+  ///
+  /// The method returns a [Future] that completes with a [String] containing
+  /// the version of the device's platform, or a [bool] indicating if the device
+  /// could not be queried.
   Future<dynamic> platformVersion() => Platform.version(this);
 
+  /// Retrieves the name of the device as a [String].
+  ///
+  /// This method sends a command to the device to retrieve its name. The
+  /// device must be connected and authenticated before this method can be
+  /// used.
+  ///
+  /// The method returns a [Future] that completes with a [String] containing
+  /// the name of the device, or a [bool] indicating if the device could not
+  /// be queried.
   Future<dynamic> getDeviceName() => Device.name(this);
 
   Future<dynamic> enableDevice() => Device.enable(this);
@@ -162,6 +228,21 @@ class ZKTeco {
 
   Future<dynamic> clearDisplay() => Device.clearLCD(this);
 
+  /// Writes a line of text to the device's LCD display.
+  ///
+  /// This method sends a command to the device to write a line of text to the
+  /// LCD display. The device must be connected and authenticated before this
+  /// method can be used.
+  ///
+  /// The [rank] parameter is the line number on the display to write to. The
+  /// value of [rank] must be between 0 and 15, inclusive.
+  ///
+  /// The [text] parameter is the string to write to the display. The string
+  /// should not be longer than 16 characters.
+  ///
+  /// The method returns a [Future] that completes with a [bool] indicating if
+  /// the device was successfully queried, or a [String] containing an error
+  /// message if the device could not be queried.
   Future<dynamic> writeDisplay(int rank, String text) =>
       Device.writeLCD(this, rank, text);
 
@@ -169,12 +250,35 @@ class ZKTeco {
 
   Future<dynamic> getTime() => Time.get(this);
 
+  /// Sets the device's time to the given [DateTime].
+  ///
+  /// The method returns a [Future] that completes with a [bool] indicating if
+  /// the device's time was successfully set, or a [String] containing an error
+  /// message if the device could not be queried.
   Future<dynamic> setTime(DateTime time) => Time.set(this, time);
 
+  /// Retrieves the SSR (Silicon Serial Number Register) of the device as a [String].
+  ///
+  /// This method sends a command to the device to retrieve its SSR. The device
+  /// must be connected and authenticated before this method can be used.
+  ///
+  /// The method returns a [Future] that completes with a [String] containing
+  /// the SSR of the device, or a [bool] indicating if the device could not be
+  /// queried.
   Future<dynamic> getSsr() => Ssr.get(this);
 
   Future<dynamic> getFingerprint(int uid) => Fingerprint.get(this, uid);
 
+  /// Retrieves all users from the device.
+  ///
+  /// This method sends a command to the device to retrieve all users. The
+  /// device must be connected and authenticated before this method can be
+  /// used.
+  ///
+  /// The method returns a [Future] that completes with a [List] containing all
+  /// users, or a [bool] indicating if the device could not be queried. The list
+  /// contains [UserInfo] objects, which have the user ID, name, role, password,
+  /// and card number. If the user name is empty, the user ID is used instead.
   Future<List<UserInfo>> getUsers() => User.get(this);
 
   /// Sets a user in the device.
@@ -225,13 +329,53 @@ class ZKTeco {
           cardNo: cardNo,
           role: role);
 
+  /// Clears all users from the device.
+  ///
+  /// This method sends a command to the device to clear all users. The device
+  /// must be connected and authenticated before this method can be used.
+  ///
+  /// The method returns a [Future] that completes with a [bool] indicating if
+  /// the users were successfully cleared, or a [String] containing an error
+  /// message if the device could not be queried.
   Future<dynamic> clearUsers() => User.clear(this);
 
+  /// Removes a user from the device.
+  ///
+  /// This method sends a command to the device to remove a user. The device
+  /// must be connected and authenticated before this method can be used.
+  ///
+  /// The method returns a [Future] that completes with a [bool] indicating if
+  /// the user was successfully removed, or a [String] containing an error
+  /// message if the device could not be queried.
+  ///
+  /// The [uid] parameter is the user ID of the user to remove. The user ID must
+  /// be between 1 and [Util.USHRT_MAX], inclusive.
   Future<dynamic> removeUser(int uid) => User.remove(this, uid);
 
+  /// Clears all administrators from the device.
+  ///
+  /// This method sends a command to the device to clear all administrators. The
+  /// device must be connected and authenticated before this method can be used.
+  ///
+  /// The method returns a [Future] that completes with a [bool] indicating if the
+  /// administrators were successfully cleared, or a [String] containing an error
+  /// message if the device could not be queried.
   Future<dynamic> clearAdmins() => User.clearAdmin(this);
 
+  /// Retrieves attendance logs from the machine.
+  ///
+  /// The method returns a [Future] that completes with a [List] of [AttendanceLog]
+  /// objects, or a [bool] indicating if the device could not be queried.
   Future<List<AttendanceLog>> getAttendanceLogs() => Attendance.get(this);
 
+  /// Clears all attendance records from the device.
+  ///
+  /// The method sends a command to the device to clear all attendance records.
+  /// The device must be connected and authenticated before this method can be
+  /// used.
+  ///
+  /// The method returns a [Future] that completes with a [bool] indicating if
+  /// the device could clear all attendance records, or a [String] containing an
+  /// error message if the device could not be queried.
   Future<dynamic> clearAttendance() => Attendance.clear(this);
 }
